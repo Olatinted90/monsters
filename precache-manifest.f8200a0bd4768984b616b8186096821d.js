7ec24b5cc0ba0fa4b7005c1af6b5c3cad041ba65
Olawale Yusuf <olatinted90@gmail.com>
2019-11-12 17:00:14 +0100

    Updates

---
+++ precache-manifest.f8200a0bd4768984b616b8186096821d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44e91908af69014627eb9c781f3b2bb",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "684713f486e178eed757",
+    "url": "/monsters/static/css/main.cb77bd82.chunk.css"
+  },
+  {
+    "revision": "849a0e77c7a65ebc01db",
+    "url": "/monsters/static/js/2.64cdb3b7.chunk.js"
+  },
+  {
+    "revision": "684713f486e178eed757",
+    "url": "/monsters/static/js/main.dbb83f13.chunk.js"
+  },
+  {
+    "revision": "a780c5024727f4e9d1d4",
+    "url": "/monsters/static/js/runtime-main.4488f325.js"
+  }
+]);
